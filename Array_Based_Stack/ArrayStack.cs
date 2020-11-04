4d71634965a68240fc99317b30790313426efff6
Hieu Dang <57908585+thanhhieu1611@users.noreply.github.com>
2020-11-04 11:18:07 -0600

    Initial upload

---
+++ Array_Based_Stack/ArrayStack.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Based_Stack
+{
+    class ArrayStack
+    {
+        #region CLASS VARIABLES
+        //what the maximum number of integers in the stack can be
+        //Assign an initial value for stack capacity
+        private int capacity = 10;
+
+        //contains the actual integers in the stack
+        private int[] stack;
+
+        //Top of stack (size)
+        private int size;
+
+        #endregion
+
+        #region PROPERTIES
+        public int Capacity
+        {
+            get => capacity;
+            set
+            {
+                capacity = value;
+            }
+        }
+
+        public int[] Stack
+        {
+            get => stack;
+            set
+            {
+                stack = value;
+            }
+        }
+
+        public int Size
+        {
+            get => size;
+            set
+            {
+                size = value;
+            }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+        //default constructor
+        public ArrayStack()
+        {
+            //Initialize an empty stack
+            stack = new int[capacity];
+
+            //Set an empty stack
+            size = -1;
+        }
+
+        //constructor with capacity parametter
+        public ArrayStack(int myCapacity)
+        {
+            //Initialize an empty stack
+            stack = new int[myCapacity];
+
+            //Set an empty stack
+            size = -1;
+        }
+        #endregion
+
+        #region METHODS
+        public void push(int x)
+        {
+            if(size + 1 == stack.Length)
+                throw new OverflowException("Need to double the current array!");
+            stack[++size] = x;
+        }
+
+        public void pop()
+        {
+            if(size == -1)
+                throw new OverflowException("Sorry, you can not pop last value because stack is empty!");
+            size--;
+            //Remove values in array, which have larger index than current top of the array
+            stack[size + 1] = 0;
+        }
+
+        public int peek()
+        {
+            if(size == -1)
+                throw new OverflowException("Top if current stack is empty!");
+            return stack[size];
+        }
+
+        #endregion
+    }
+}
---
+++ Array_Based_Stack/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Based_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Create an empty default arrayStack");
+                ArrayStack myArrayStack = new ArrayStack();
+
+                Console.WriteLine("Test default values of top and array");
+                PrintArrayStackElements(myArrayStack);
+
+                Console.WriteLine("Push first integer into myArrayStack");
+                myArrayStack.push(1);
+
+                Console.WriteLine("Push second integer into myArrayStack");
+                myArrayStack.push(2);
+
+                Console.WriteLine("Push third integer into myArrayStack");
+                myArrayStack.push(3);
+
+                Console.WriteLine("Current top of myArrayStack is " + myArrayStack.peek() + Environment.NewLine);
+
+                Console.WriteLine("Test values of top and array after push 1 into the stack");
+                PrintArrayStackElements(myArrayStack);
+
+                Console.WriteLine("Pop first integer from myArrayStack");
+                myArrayStack.pop();
+
+                Console.WriteLine("Pop second integer from myArrayStack");
+                myArrayStack.pop();
+
+                Console.WriteLine("Current top of myArrayStack is " + myArrayStack.peek() + Environment.NewLine);
+
+                Console.WriteLine("Test values of top and array after pop 1 from the stack");
+                PrintArrayStackElements(myArrayStack);
+
+            }
+            catch (OverflowException uex)
+            {
+                Console.WriteLine(uex.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+
+        #region Methods
+        //Print stack elements
+        static void PrintArrayStackElements(ArrayStack myStack)
+        {
+            Console.WriteLine("Index of current Top of stack: " + myStack.Size);
+            for (int i = myStack.Stack.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Integer " + i + ": " + myStack.Stack[i]);
+            }
+            Console.WriteLine("");
+        }
+
+        #endregion
+
+    }
+}
